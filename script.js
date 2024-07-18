823626fb1f63073e8b62a7d5a6038b923ed1b0e3
chandru sivakumar <chandrusivkumar16@gmail.com>
2024-07-18 17:09:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const input = document.querySelector("input"),
+  guess = document.querySelector(".guess"),
+  checkButton = document.querySelector("button"),
+  remainChances = document.querySelector(".chances");
+  
